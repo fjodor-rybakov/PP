1e5d8cd97e80eae2b863ab8c10bc399c42b21108
fjodor-rybakov <fedryb@mail.ru>
2019-02-17 17:47:10 +0300

    update calc any size matrix

--- PPLab1Consistantly/PPLab1Consistantly.cpp
+++ PPLab1Consistantly/PPLab1Consistantly.cpp
@@ -8,29 +8,84 @@
 
 using namespace std;
 
-const int n = 3;
-int matrix[n][n] = { 1, -2, 0, 4, 6, 2, -3, 4, -2 };
+/* const int n = 4;
+int matrix[n][n] = {
+	1, -2, 0, 3,
+	6, -4, -3, -1,
+	-1, 7, 9, 4,
+	1, -2, 2, -6
+};*/
 
-int getMinor(int k, int l) {
-	vector<int> minor;
+/*int n = 0;
+vector<vector<int>> matrix(n, vector<int>(n));*/
 
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			if ((i != k) && (j != l))
-				minor.push_back(matrix[i][j]);
-
-	return minor[0] * minor[3] - minor[1] * minor[2];
+int getDeterm(vector<vector<int>> matr, int minorSize) {
+	int sum, mul;
+	sum = 0;
+	if (2 == minorSize)
+	{
+		sum = matr[0][0] * matr[1][1] - matr[0][1] * matr[1][0];
+		return sum;
+	}
+	for (int i = 0; i < minorSize; i++)
+	{
+		mul = 1;
+		int k = i;
+		for (int j = 0; j < minorSize; j++)
+		{
+			mul *= matr[j][k];
+			k = k == minorSize - 1 ? 0 : k + 1;
+		}
+		sum += mul;
+	}
+	for (int i = 0; i < minorSize; i++)
+	{
+		mul = 1;
+		int k = i;
+		for (int j = 0; j < minorSize; j++)
+		{
+			mul *= matr[j][k];
+			k = k == 0 ? minorSize - 1 : k - 1;
+		}
+		sum -= mul;
+	}
+	return sum;
 }
 
-void getAdditions() {
-	for (int k = 0; k < n; k++) {
-		for (int l = 0; l < n; l++) {
+int getMinor(int k, int l, vector<vector<int>> matrix, int size)
+{
+	int minorSize = size - 1, x = 0, y = 0;
+	vector<vector<int>> minor(minorSize, vector<int>(minorSize));
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if ((i != k) && (j != l))
+			{
+				minor[x][y] = matrix[i][j];
+				y++;
+				if (y % minorSize == 0)
+				{
+					x++;
+					y = 0;
+				}
+			}
+		}
+	}
+
+	return getDeterm(minor, minorSize);
+}
+
+void getAdditions(vector<vector<int>> matrix, int size) {
+	for (int k = 0; k < size; k++) {
+		for (int l = 0; l < size; l++) {
 			if ((k + l) % 2 == 0) {
-				cout << getMinor(k, l) << " ";
+				cout << getMinor(k, l, matrix, size) << " ";
 			}
 			else
 			{
-				cout << -getMinor(k, l) << " ";
+				cout << -getMinor(k, l, matrix, size) << " ";
 			}
 		}
 
@@ -40,19 +95,22 @@ void getAdditions() {
 
 int main()
 {
-	/*int value;
-	cout << "Enter pls matrix 3x3" << endl;
-	for (int i = 0; i < n; i++)
+	int value = 0, size = 0;
+	cout << "Enter pls size matrix" << endl;
+	cin >> size;
+	vector<vector<int>> matrix(size, vector<int>(size));
+	cout << "Enter pls matrix " << size << "x" << size << endl;
+
+	for (int i = 0; i < size; i++)
 	{
-		for (int j = 0; j < n; j++)
+		for (int j = 0; j < size; j++)
 		{
 			cin >> value;
 			matrix[i][j] = value;
 		}
-	}*/
+	}
+
 	int t = clock();
-
-	getAdditions();
-
+	getAdditions(matrix, size);
 	cout << clock() - t << " ms" << endl;
 }
