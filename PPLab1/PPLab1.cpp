75ae2d11ec93c5da780714d908aa86e605b9c583
fjodor-rybakov <fedryb@mail.ru>
2019-02-17 16:21:09 +0300

    add 1lab consistantly

---
+++ PPLab1/PPLab1.cpp
@@ -0,0 +1,56 @@
+﻿// PPLab1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include <iostream>
+
+const int n = 3;
+int a[n][n] = { 1, -2, 0, 4, 6, 2, -3, 4, -2 };
+int b[n][n] = { 0, 2, 0, 1, 1, 1, 5, -3, 10 };
+int c[n][n];
+
+DWORD WINAPI ThreadFunc(PVOID pvParam)
+{
+	int num, i, j;
+	num = (*((int *)pvParam));
+	for (i = 0; i < n; i++)
+	{
+		c[num][i] = 0;
+		for (j = 0; j < n; j++) c[num][i] += a[num][j] * b[j][i];
+	}
+
+	DWORD dwResult = 0;
+	return dwResult;
+}
+
+int main()
+{
+	int i, j;
+	int x[n];
+
+	DWORD dwThreadId[n - 1], dw;
+	HANDLE hThread[n - 1];
+
+	for (i = 1; i < n; i++)
+	{
+		x[i] = i;
+		hThread[i - 1] = CreateThread(NULL, 0, ThreadFunc, (PVOID)&x[i], 0, &dwThreadId[i - 1]);
+		if (!hThread) printf("main process: thread %d not execute!", i);
+	}
+	
+	x[0] = 0;
+	ThreadFunc((PVOID)&x[0]);
+
+	dw = WaitForMultipleObjects(n - 1, hThread, TRUE, INFINITE);
+
+	for (i = 0; i < n; i++)
+	{
+		fprintf(stdout, "\n");
+		for (j = 0; j < n; j++) printf("%d ", c[i][j]);
+	}
+
+	return 0;
+}
---
+++ PPLab1Consistantly/PPLab1Consistantly.cpp
@@ -0,0 +1,53 @@
+﻿// PPLab1Consistantly.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int n = 3;
+int matrix[n][n] = { 1, -2, 0, 4, 6, 2, -3, 4, -2 };
+
+int getMinor(int k, int l) {
+	vector<int> minor;
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			if ((i != k) && (j != l))
+				minor.push_back(matrix[i][j]);
+
+	return minor[0] * minor[3] - minor[1] * minor[2];
+}
+
+void getAdditions() {
+	for (int k = 0; k < n; k++) {
+		for (int l = 0; l < n; l++) {
+			if ((k + l) % 2 == 0) {
+				cout << getMinor(k, l) << " ";
+			}
+			else
+			{
+				cout << -getMinor(k, l) << " ";
+			}
+		}
+
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int value;
+	cout << "Enter pls matrix 3x3" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> value;
+			matrix[i][j] = value;
+		}
+	}
+	getAdditions();
+}
