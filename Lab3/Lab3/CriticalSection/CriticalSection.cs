47fddfa2a7eff84b65191b9131a14f126a9d6cef
fjodor-rybakov <fedryb@mail.ru>
2019-05-07 15:27:00 +0300

    add lab3

---
+++ Lab3/Lab3/CriticalSection/CriticalSection.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Threading;
+
+namespace Lab3.CriticalSection
+{
+    public class CriticalSection : ICriticalSection
+    {
+        private static Mutex Mut { get; } = new Mutex();
+        private static uint _spinCount;
+
+        public CriticalSection(uint count)
+        {
+            _spinCount = count;
+        }
+        
+        public void Enter()
+        {
+            var numAttempt = 0;
+            if (Mut.WaitOne()) return;
+            while (!Mut.WaitOne())
+            {
+                numAttempt++;
+                if (numAttempt != _spinCount) continue;
+                numAttempt = 0;
+                Thread.Sleep(10);
+            }
+        }
+
+        public bool TryEnter(int timeout)
+        {
+            var numAttempt = 0;
+            var startTime = DateTime.Now.Millisecond;
+            if (Mut.WaitOne()) return true;
+            while (!Mut.WaitOne())
+            {
+                numAttempt++;
+                if (DateTime.Now.Millisecond >= startTime + timeout) return false;
+                if (numAttempt != _spinCount) continue;
+                numAttempt = 0;
+                Thread.Sleep(10);
+            }
+
+            return true;
+        }
+
+        public void SetSpinCount(uint count)
+        {
+            _spinCount = count;
+        }
+
+        public void Leave()
+        {
+            Mut.ReleaseMutex();
+        }
+    }
+}
---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Lab3.Assets;
+
+namespace Lab3
+{
+    internal static class Program
+    {
+        private static double _pi;
+        private const int CountThreads = 4;
+
+        private static CriticalSection.CriticalSection _criticalSection;
+        private static int _timeout;
+
+        public static void Main(string[] args)
+        {
+            if (args[0] == null && args[1] == null && args[2] == null)
+            {
+                Console.WriteLine("Invalid number of arguments. Must: Lab3.exe <count steps> <timeout> <count try cs>");
+                return;
+            }
+
+            if (!int.TryParse(args[0], out var countSteps))
+            {
+                Console.WriteLine("Incorrect value steps. Value must be a number");
+                return;
+            }
+            
+            if (!int.TryParse(args[1], out _timeout))
+            {
+                Console.WriteLine("Incorrect value timeout. Value must be a number");
+                return;
+            }
+            
+            if (!uint.TryParse(args[2], out var countTry))
+            {
+                Console.WriteLine("Incorrect value count try. Value must be a number");
+                return;
+            }
+            
+            _criticalSection = new CriticalSection.CriticalSection(countTry);
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            var handles = CreateThreads(countSteps, TryType.TryEnter);
+            foreach (var tr in handles) tr.Join();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine("Pi: " + _pi + ", Work time: " + elapsedMs + "ms");
+            _pi = 0;
+
+            watch = System.Diagnostics.Stopwatch.StartNew();
+
+            handles = CreateThreads(countSteps, TryType.Enter);
+            foreach (var tr in handles) tr.Join();
+
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine("Pi: " + _pi + ", Work time: " + elapsedMs + "ms");
+        }
+
+        private static IEnumerable<Thread> CreateThreads(int countSteps, string tryType)
+        {
+            var waitHandles = new List<Thread>();
+            var countStepsPerThread = countSteps / CountThreads;
+            for (var i = 0; i < CountThreads; i++)
+            {
+                var newThread = new Thread(CalcPi);
+                newThread.Start(new ThreadContext
+                {
+                    Left = i * countStepsPerThread,
+                    Right = (i + 1) * countStepsPerThread,
+                    Step = 1.0 / countSteps,
+                    Name = $"Thread: {i}",
+                    TryType = tryType
+                });
+                waitHandles.Add(newThread);
+            }
+
+            return waitHandles;
+        }
+
+        private static void CalcPi(object param)
+        {
+            var threadContext = (ThreadContext) param;
+            for (var i = threadContext.Left; i < threadContext.Right; i++)
+            {
+                var x = (i + 0.5) * threadContext.Step;
+                if (threadContext.TryType == TryType.Enter) _criticalSection.Enter();
+                else
+                {
+                    while (!_criticalSection.TryEnter(_timeout))
+                    {
+                    }
+                }
+
+                _pi += 4.0 / (1.0 + x * x) * threadContext.Step;
+                _criticalSection.Leave();
+            }
+        }
+    }
+}
